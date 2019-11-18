43617ff5205a932507eb16778c98d1dc9fb65bf9
MAMUN1 <mamunsad@gmail.com>
2019-11-18 04:09:15 +0400

    Css 2 Slider round animation and clicking added

---
+++ CSS/image slider/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>My Slider</title>
+    <link rel="stylesheet" href="style.css" >
+    <style>
+    
+    </style>
+</head>
+<body>
+    <h1>MAMWINGS SLIDER</h1>
+    <div class="content-all">
+<div class="content-mam">
+<figure><img src="images/img(0).jpg" alt="1"></figure>
+ <figure><img src="images/img(2).jpg" alt="2"></figure>
+ <figure><img src="images/img(2).png" alt="3"></figure>
+ <figure><img src="images/img(3).png" alt="4"></figure>
+ <figure><img src="images/img(1).jpg" alt="5"></figure>
+ <figure><img src="images/img(4).png" alt="6"></figure>
+ <figure><img src="images/img(1).jpg" alt="7"></figure>
+ <figure><img src="images/img(0).png" alt="8"></figure>
+ <figure><img src="images/img(1).jpg" alt="9"></figure>
+ <figure><img src="images/img(3).png" alt="10"></figure>
+ 
+        </div>
+    </div>
+</body>
+</html>
